a95a69c0d646271d7d033ea3a08c1aeb062410a3
Rebecca16Celine <rebecca16celine@gmail.com>
2026-08-26 15:24:02 +0000

    Implement HyperLogLog

--- backend/testSketches.js
+++ backend/testSketches.js
@@ -9,3 +9,19 @@ bloom.add("request-3");
 console.log("request-1:", bloom.mightContain("request-1"));
 console.log("request-2:", bloom.mightContain("request-2"));
 console.log("request-999:", bloom.mightContain("request-999"));
+
+const HyperLogLog = require("./hyperLogLog");
+
+const hll = new HyperLogLog();
+
+for (let i = 0; i < 7000; i++) {
+    hll.add(`request-${i}`);
+}
+
+for (let i = 0; i < 3000; i++) {
+    hll.add(`request-${i}`);
+}
+
+console.log("Total requests: 10000");
+console.log("Actual unique: 7000");
+console.log("HLL estimate:", hll.estimate());
